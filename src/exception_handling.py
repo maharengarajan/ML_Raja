f0dd0045f427c8f013327baae81f984a8d168d60
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-05-10 13:07:03 +0530

    Exception Updated

--- src/exception_handling.py
+++ src/exception_handling.py
@@ -10,9 +10,10 @@ def error_message_details(error,error_details:sys):
 
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error_message,error_details:sys):
         super().__init__(error_message)
-        self.error_message = error_message_details(error_message,error_detail=error_detail)
+        self.error_message = error_message_details(error_message,error_details=error_details)
     
     def __str__(self):
         return self.error_message
+    
