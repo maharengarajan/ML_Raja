7d7db89691833b9ffeca2e7f8a674922b779d97c
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-05-23 17:39:17 +0530

    egg info updated in requirements.txt

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-from setuptools import find_packages,setup 
-from typing import List 
+from setuptools import find_packages,setup
+
+from typing import List
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements() ->List[str]:
+    
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
 
 
 
-def get_requirements(file_path:str)->List[str]:
-    """This fun will return the list of requirements"""
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("/n", "")  for req in requirements]
-
-    return requirements
 setup(
-    name = "ML_Raja",
-    version="0.0.1",
-    author = "Renga Rajan k",
+    name = "Renga Rajan",
+    version = "0.0.1",
+    author = "Raja",
+    author_email = "maharengarajan46@gmail.com",
     packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')
-    )
+    install_requires = get_requirements()
+)
--- src/exception_handling.py
+++ src/exception_handling.py
@@ -1,4 +1,6 @@
 import sys
+from src.logger import logging
+
 
 def error_message_details(error,error_details:sys):
     _,_,exc_tb = error_details.exc_info()
@@ -16,4 +18,14 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+    
+    
+if __name__=="__main__":
+    try:
+        logging.info("Div by zero error")
+        a = 1/0
+    except Exception as e:
+        logging.info("Division by zero error")
+        raise CustomException(e,sys)
     
